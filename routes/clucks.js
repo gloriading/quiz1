0b1068cb5fed8edc6b4c5121b5b172ed51854ad1
Gloria Ding <gloria.hc.ding@gmail.com>
2017-12-05 16:34:46 -0800

    change from url image to uploads

--- routes/clucks.js
+++ routes/clucks.js
@@ -4,10 +4,10 @@ const router = express.Router();
 const knex = require('../db');
 
 // ---------------------------------------------------------------------
-// const multer = require('multer');
+const multer = require('multer');
 const path = require('path');
-// const UPLOADS_DIR = 'uploads';
-// const upload = multer({dest: path.join(__dirname, '..', 'public', UPLOADS_DIR)});
+const UPLOADS_DIR = 'uploads';
+const upload = multer({dest: path.join(__dirname, '..', 'public', UPLOADS_DIR)});
 // ---------------------------------------------------------------------
 
 router.get('/new', (request, response) => { //real path: cohort/new
@@ -15,10 +15,12 @@ router.get('/new', (request, response) => { //real path: cohort/new
 });
 
 // '/' has to match the form action="/clucks"
-router.post('/', (request, response) => {
+router.post('/', upload.single('picture') , (request, response) => {
   const username = request.body.username;
-  const image_url = request.body.image_url;
+  // const image_url = request.body.image_url;
   const content = request.body.content;
+  const filename = request.file.filename;
+  const image_url = path.join(UPLOADS_DIR, filename);
 
     knex
       .insert({username, image_url, content})
